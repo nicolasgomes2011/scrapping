26466cfe0b074b148bd6634dbd69de667833376e
nicolas <gomes.nicolas.2011@gmail.com>
2025-06-05 20:03:45 -0300

    Remove scrappingremove.py and teste.py files as they are no longer needed in the project.

--- teste.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.cpfl.com.br/login"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify()[:1000])  # imprime os primeiros 1000 chars
-form = soup.find('form')
-print(form)
